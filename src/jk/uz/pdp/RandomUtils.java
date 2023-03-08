6281ab5d8f1b1b2ee6561d143cc57099050cfe32
Bekzod <bekzodsadriddinov92@gmail.com>
2023-03-08 19:49:48 +0500

    Initial commit

---
+++ src/jk/uz/pdp/RandomUtils.java
@@ -0,0 +1,22 @@
+package jk.uz.pdp;
+
+import java.util.Random;
+
+public class RandomUtils {
+
+    public static byte getMaxAge() {
+        return (byte) new Random().nextInt(50, 100);
+    }
+
+    public static int getDateNumber(){
+        return new Random().nextInt(1,10);
+    }
+
+    public static int getFishNumber(){
+        return new Random().nextInt(1,3);
+    }
+
+    public static int getAquariumSize(){
+        return new Random().nextInt(20,30);
+    }
+}
